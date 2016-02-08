60b8e86ec98ef8ea58c30185a2d76ff422bfcaa0
noguhiro <g007c1280@gmail.com>
2016-02-09 03:26:37 +0900

    bug fixed, respond to version 5.3

--- tests/DescTest.php
+++ tests/DescTest.php
@@ -16,10 +16,10 @@ class DescTest extends \PHPUnit_Framework_TestCase
      */
     public function testMajorVersion()
     {
-        $versions = ['2.0.0', '1.0.0', '40.0.0', '3.0.0'];
+        $versions = array('2.0.0', '1.0.0', '40.0.0', '3.0.0');
         $vs = new VersionSort($versions);
         $expected = $vs->desc();
-        $actual = ['40.0.0', '3.0.0', '2.0.0', '1.0.0'];
+        $actual = array('40.0.0', '3.0.0', '2.0.0', '1.0.0');
         $this->assertEquals($expected, $actual);
     }
 
@@ -28,10 +28,10 @@ class DescTest extends \PHPUnit_Framework_TestCase
      */
     public function testSubVersion()
     {
-        $versions = ['1.1.0', '1.0.0', '1.299.0', '1.2.0'];
+        $versions = array('1.1.0', '1.0.0', '1.299.0', '1.2.0');
         $vs = new VersionSort($versions);
         $expected = $vs->desc();
-        $actual = ['1.299.0', '1.2.0', '1.1.0', '1.0.0'];
+        $actual = array('1.299.0', '1.2.0', '1.1.0', '1.0.0');
         $this->assertEquals($expected, $actual);
     }
 
@@ -40,10 +40,10 @@ class DescTest extends \PHPUnit_Framework_TestCase
      */
     public function testMinorVersion()
     {
-        $versions = ['1.0.100', '1.0.1', '1.0.20', '1.0.9'];
+        $versions = array('1.0.100', '1.0.1', '1.0.20', '1.0.9');
         $vs = new VersionSort($versions);
         $expected = $vs->desc();
-        $actual = ['1.0.100', '1.0.20', '1.0.9', '1.0.1'];
+        $actual = array('1.0.100', '1.0.20', '1.0.9', '1.0.1');
         $this->assertEquals($expected, $actual);
     }
 
@@ -52,18 +52,18 @@ class DescTest extends \PHPUnit_Framework_TestCase
      */
     public function testAllVersion()
     {
-        $versions = [
+        $versions = array(
             '1203.1.RC.100', '1203.1.RC.21', '1.0.20',
             '1.0.239', '1.1.alpha.1', '1.1.alpha.4',
             '3.0.2', '3.8.1'
-        ];
+        );
         $vs = new VersionSort($versions);
         $expected = $vs->desc();
-        $actual = [
+        $actual = array(
             '1203.1.RC.100', '1203.1.RC.21', '3.8.1',
             '3.0.2', '1.1.alpha.4', '1.1.alpha.1',
             '1.0.239', '1.0.20'
-        ];
+        );
         $this->assertEquals($expected, $actual);
     }
 
